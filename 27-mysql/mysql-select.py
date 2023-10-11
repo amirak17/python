1249c5e8a5be8dc1dbaa75a56ef54c7700eba2b4
Amir Ali Khan <amir.aakhan@gmail.com>
2023-10-11 12:11:04 -0400

    table correction

--- 27-mysql/mysql-select.py
+++ 27-mysql/mysql-select.py
@@ -4,7 +4,7 @@ import mysql.connector
 db = mysql.connector.connect(host="localhost", user="test", password="pwd", database="test")
 cursor = db.cursor(buffered=True, dictionary=True)
 
-sql = 'SELECT * FROM symbols'
+sql = 'SELECT * FROM stocks'
 cursor.execute(sql)
 rows = cursor.fetchall()
 
